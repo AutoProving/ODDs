cc283db50f1f4cf51583d5ebac65339be36e918d
omlie <lieolemagnus@gmail.com>
2019-02-25 15:01:51 +0100

    finish input, awaiting comments

--- reading.c
+++ reading.c
@@ -1,96 +1,132 @@
-// Copyright 2019 YOUR NAME
-// This file is licensed under MIT License, as specified in the file LISENSE located at the root folder of this repository.
+// Copyright 2019 Ole Magnus Lie
+// This file is licensed under MIT License, as specified in the file LICENSE located at the root folder of this repository.
 
 #include <stdlib.h>
+#include <string.h>
 #include "odd.h"
 
-NumSymbol readSymbol(char* filename){
-    FILE *reader = fopen(filename, 'r');
-    NumSymbol symbol;
-    fscanf(reader, "%d", &symbol);
-    fclose(reader);
-    return symbol;
+void readSymbol(FILE *reader, NumSymbol *symbol){
+    fscanf(reader, "%d", symbol);
 } // Reads a string representing the name of a symbol
 
 
-void readAlphabetMap(char* filename, int nSymbols, AlphabetMap* map){
-    map->sizeAlphabet = nSymbols;
-    FILE *reader = fopen(filename, 'r');
-    int number;
-    char *string;
-    for (int i = 0; i < nSymbols; i++){
-        fscanf(reader, "%d %s", &number, string);
-        map->N2S[i] = string;
-        map->S2N[i] = number;
+void readAlphabetMap(FILE *reader, AlphabetMap* map){
+    char line[64], c[32];
+    while(fgets(line, sizeof(line), reader))
+        if (strstr(line, "ALPHABET_MAP") != NULL)
+            break;
+
+    sscanf(line, "%s %d", c, &map->sizeAlphabet);
+    map->S2N = malloc(map->sizeAlphabet * sizeof(int));
+    map->N2S = malloc(map->sizeAlphabet * sizeof(char*));
+
+    for (int i = 0; i < map->sizeAlphabet; i++){
+        fscanf(reader, "%d %s", map->S2N, *map->N2S);
     }
-    fclose(reader);
 } // Reads nSymbols
 
 
-State readState(char* filename){
-    FILE *reader = fopen(filename, 'r');
-    State state;
-    fscanf(reader, "%d", &state);
-    fclose(reader);
-    return state;
+void readState(FILE *reader, State *state){
+    fscanf(reader, "%d", state);
 } // Reads a state (represented by an int)
 
 
-void readStates(char* filename, int nStates, StateContainer* states){
-    states->nStates = nStates;
-    for (int i = 0; i < nStates; i++){
-        states->set[i] = readState(filename);
+void readStates(FILE *reader, StateContainer* states){
+    char line[64], c[32];
+    while(fgets(line, sizeof(line), reader))
+        if (strstr(line, "STATES") != NULL)
+            break;
+
+    sscanf(line, "%s %d", c, &states->nStates);
+    states->set = malloc(states->nStates * sizeof(State*));
+    for (int i = 0; i < states->nStates; i++){
+        State state;
+        readState(reader, &state);
+        states->set[i] = state;
     }
 }  // Reads nStates
 
 
-Transition readTransition(char* filename){
-    Transition t;
-    t.s1 = readState(filename);
-    t.a = readSymbol(filename);
-    t.s2 = readState(filename);
-    return t;
-
+void readTransition(FILE *reader, Transition *transition){
+    readState(reader, &transition->s1);
+    readSymbol(reader, &transition->a);
+    readState(reader, &transition->s2);
 } // reads a transition represented by a triple of integers
 
 
-void readTransitions(char* filename, int nTransitions, TransitionContainer* transitions){
-    transitions->nTransitions = nTransitions;
-    for(int i = 0; i < nTransitions; i++){
-        transitions->set[i] = readTransition(filename);
+void readTransitions(FILE *reader, TransitionContainer* transitions){
+    char line[64], c[32];
+    while(fgets(line, sizeof(line), reader))
+        if (strstr(line, "TRANSITIONS") != NULL)
+            break;
+
+    sscanf(line, "%s %d", c, &transitions->nTransitions);
+    transitions->set = malloc(transitions->nTransitions * sizeof(Transition));
+    for(int i = 0; i < transitions->nTransitions; i++){
+        Transition transition;
+        readTransition(reader, &transition);
+        transitions->set[i] = transition;
     }
 } // Reads nTransitions transitions
 
 
-Layer readLayer(char* filename){
+void readLayer(FILE *reader, Layer* layer){
     AlphabetMap map;
     StateContainer leftStates, initialStates, rightStates, finalStates;
     TransitionContainer transitions;
 
-    readAlphabetMap(filename, 5, &map);
-    readStates(filename, 5, &leftStates);
-    readStates(filename, 5, &rightStates);
-    readStates(filename, 5, &initialStates);
-    readStates(filename, 5, &finalStates);
-    readTransitions(filename, 5, &transitions);
+    char line[64], c[32];
+    while(fgets(line, sizeof(line), reader))
+        if (strstr(line, "LAYER") != NULL)
+            break;
 
-    Layer l = {.map = map,
-               .leftStates = leftStates,
-               .initialStates = initialStates,
-               .rightStates = rightStates,
-               .finalStates = finalStates,
-               .width = leftStates.nStates > rightStates.nStates ? leftStates.nStates : rightStates.nStates,
-               .initialFlag = 0,
-               .finalFlag = 0};
+    readAlphabetMap(reader, &map);
+    readStates(reader, &leftStates);
+    readStates(reader, &rightStates);
+    readStates(reader, &initialStates);
+    readStates(reader, &finalStates);
+    readTransitions(reader, &transitions);
 
-    return l;
+    layer->map = map;
+    layer->leftStates = leftStates;
+    layer->rightStates = rightStates;
+    layer->initialStates = initialStates;
+    layer->finalStates = finalStates;
+    layer->width = leftStates.nStates > rightStates.nStates ? leftStates.nStates : rightStates.nStates;
+
+    while(fgets(line, sizeof(line), reader)) {
+        int flag;
+        if (strstr(line, "INITIAL_FLAG") != NULL) {
+            sscanf(line, "%s %d", c, &flag);
+            layer->initialFlag = flag == 1 ? true : false;
+        }
+        if (strstr(line, "FINAL_FLAG") != NULL) {
+            sscanf(line, "%s %d", c, &flag);
+            layer->finalFlag = flag == 1 ? true : false;
+        }
+    }
 } // Reads a layer
 
 
 void readODD(char* filename, ODD* odd){
-    odd->nLayers = 5;
-    odd->width = 5;
+    FILE *reader = fopen(filename, "r");
+    char line[64], c[32];
+
+    while(fgets(line, sizeof(line), reader))
+        if (strstr(line, "ODD") != NULL)
+            break;
+
+    sscanf(line, "%s %d", c, &odd->nLayers);
+
+    odd->width = 0;
+    odd->layerSequence = malloc(odd->nLayers * sizeof(Layer));
+
     for(int i = 0; i < odd->nLayers; i++){
-        odd->layerSequence[i] = readLayer(filename);
+        Layer layer;
+        readLayer(reader, &layer);
+
+        odd->layerSequence[i] = layer;
+        odd->width = odd->layerSequence[i].width > odd->width ? odd->layerSequence[i].width : odd->width;
     }
+    fclose(reader);
 } // Reads a sequence of layers
