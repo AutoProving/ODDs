1eaf2534373c851961b0dfeb1af906dd6fc4e938
Andre Berge <andre.bergeoot@gmail.com>
2019-02-25 12:02:50 +0100

    Added the function-names to printing.h - branching

--- printing.c
+++ printing.c
@@ -1,3 +1,77 @@
 // Copyright 2019 YOUR NAME
 // This file is licensed under MIT License, as specified in the file LISENSE located at the root folder of this repository.
 
+#include "odd.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("HELLO\n");
+	return 0;
+}
+
+
+/**
+	Will print a number n and its corresponding string N2S[n].
+*/
+void printSymbol(char* filename, NumSymbol s)
+{
+
+}
+
+/**
+	Calls printSymbol for each element in the map.
+*/
+void printAlphabetMap(char* filename, AlphabetMap* map)
+{
+
+}
+
+/**
+	Prints a single state to file: filename.
+*/
+void printState(char* filename, State s)
+{
+
+}
+
+/**
+	Calls printState for each state.
+*/
+void printStates(char* filename, StateContainer* states)
+{
+
+}
+
+/**
+	Prints a single transition to file: filename.
+*/
+void printTransition(char* filename, Transition t)
+{
+
+} 
+
+/**
+	Calls printTransition for each transition.
+*/
+void printTransitions(char* filename, TransitionContainer* transitions)
+{
+
+}
+
+/**
+	Will call printAlphabetMap, printStates, and printTransitions.
+*/
+void printLayer(char* filename, Layer* layer)
+{
+
+}
+
+/**
+	Will call printlayer a number of times.
+*/
+void printODD(char* filename, ODD* odd)
+{
+
+}
+
