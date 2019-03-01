fcdb5839315a0d71fff89b5c528030e84e74f743
Ducktuba <anthomm@live.com>
2019-03-01 14:52:13 +0100

    Testing CLion version control

--- memorization.c
+++ memorization.c
@@ -1,3 +1,7 @@
-// Copyright 2019 YOUR NAME
+// Copyright 2019 Andreas Ommundsen
 // This file is licensed under MIT License, as specified in the file LISENSE located at the root folder of this repository.
 
+#include <stdlib.h>
+#include "odd.h"
+
+// TESTING THIS BRANCH WITH NO SURVIVORS
