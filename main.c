570e578cd475188622027dfbe64e4b008e0ced32
aom035 <aom035@student@uib.no>
2019-03-04 01:07:44 +0100

    memODD functionality, initial/final states handled, need sleep...

--- main.c
+++ main.c
@@ -6,26 +6,20 @@
 #include "reading.c"
 #include "memorization.c"
 
-int main(){
+int main() {
 
     char filename[] = "ODD-Example.txt";
     ODD *odd = malloc(sizeof(ODD));
+    ODD *oddResult = malloc(sizeof(ODD));
     readODD(filename, odd);
-//    showODD(odd);
 
-    Layer *inputLayer = &odd->layerSequence[0];
-    Layer *resultLayer = malloc(sizeof(Layer));
-
-    printf("\ninputLayer BEFORE memorizeLayer(inputLayer, resultLayer);\n\n");
-    showLayer(inputLayer);
-    memorizeLayer(inputLayer, resultLayer);
-
-    printf("\nresultLayer AFTER memorizeLayer(inputLayer, resultLayer);\n\n");
-    showLayer(resultLayer);
-
-
-    printf("\n\n-----Show ODD-----\n\n");
+    printf("----BEFORE MEMORIZE-----\n\n");
     showODD(odd);
+
+    memorizeODD(odd, oddResult);
+
+    printf("----AFTER MEMORIZE-----\n\n");
+    showODD(oddResult);
 
     return 0;
 }
--- memorization.c
+++ memorization.c
@@ -8,7 +8,7 @@
  *      *layer : The layer whose number of states we are interested in.
  *      left : True for the left side StateContainer of the layer, false for the right side StateContainer.
  * Returns:
- *      i.side * |S| : The the new size of the given stateContainer
+ *      The the new size of the given stateContainer = i.side * |S|
  * Where:
  *      i.side : the number of states on the given side.
  *      |S| : The size of the layer's alphabet.
@@ -23,6 +23,15 @@ int expandedSetSize(Layer *layer, bool left);
  *      Memory of *states is freed and then a new array holding states [0 ... newSize-1] is set to that memory.
  */
 void updateStateContainer(StateContainer *states, int newSize);
+
+/*
+ * Input:
+ *      *states : Either the layer.initialStates or layer.finalStates to be updated.
+ *      alphabetSize : The layer.map.sizeAlphabet.
+ * Effect:
+ *      *states should be updated to match the expanded state numbers.
+ */
+void updateInitialFinal(StateContainer *states, int alphaSize);
 
 void memorizeLayer(Layer *layer, Layer *result) {
 
@@ -40,6 +49,11 @@ void memorizeLayer(Layer *layer, Layer *result) {
     clonedLayer.rightStates.nStates = newRightSize;
     updateStateContainer(&clonedLayer.rightStates, newRightSize);
 
+    if (clonedLayer.initialFlag)
+        updateInitialFinal(&clonedLayer.initialStates, clonedLayer.map.sizeAlphabet);
+
+    if (clonedLayer.finalFlag)
+        updateInitialFinal(&clonedLayer.finalStates, clonedLayer.map.sizeAlphabet);
 
     free(result);
     *result = *cP;
@@ -47,11 +61,26 @@ void memorizeLayer(Layer *layer, Layer *result) {
 
 
 void memorizeODD(ODD *odd, ODD *result) {
-    printf("You've reached %s() in %s through %s, but there's nothing here yet!", __func__, __FILE__, __BASE_FILE__);
+
+    ODD clonedODD = *odd;
+    ODD *cP = &clonedODD;
+
+    for (int i = 0; i < odd->nLayers; ++i) {
+
+        Layer *inputLayer = &odd->layerSequence[i];
+        Layer *resultLayer = malloc(sizeof(Layer));
+
+        memorizeLayer(inputLayer, resultLayer);
+
+//        free(inputLayer); // TODO CAUSES SIGSEGV, FIGURE IT OUT
+        *inputLayer = *resultLayer;
+    }
+    free(result);
+    *result = *cP;
 }
 
 
-int expandedSetSize(Layer *layer, bool left){
+int expandedSetSize(Layer *layer, bool left) {
 
     int stateSize = left ? layer->leftStates.nStates : layer->rightStates.nStates;
     int alphaSize = layer->map.sizeAlphabet;
@@ -59,11 +88,35 @@ int expandedSetSize(Layer *layer, bool left){
     return expandedSize;
 }
 
-void updateStateContainer(StateContainer *states, int newSize){
+void updateStateContainer(StateContainer *states, int newSize) {
 
     free(states->set);
     states->set = malloc(newSize * sizeof(State));
     for (int i = 0; i < newSize; ++i) {
         states->set[i] = i;
     }
+}
+
+void updateInitialFinal(StateContainer *states, int alphaSize) {
+
+    int oldSetSize = states->nStates;
+    int newSetSize = states->nStates * alphaSize;
+    State *newSet = malloc(newSetSize * sizeof(State));
+
+    for (int i = 0; i < oldSetSize; ++i) {
+        states->set[i] *= alphaSize;
+        newSet[i * alphaSize] =
+                states->set[i] == 0 ?
+                0 :
+                states->set[i];
+    }
+
+    for (int l = 0; l < newSetSize; ++l) {
+        if (l % alphaSize != 0)
+            newSet[l] = newSet[l - 1] + 1;
+    }
+
+    states->nStates = newSetSize;
+    free(states->set);
+    states->set = newSet;
 }
