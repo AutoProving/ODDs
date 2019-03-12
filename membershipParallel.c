c18542b23675ca658c1bcde6d26b35926dbdeb5f
steinvik <steinvik@protours.no>
2019-03-12 17:14:31 +0100

    parallelize numMembership

--- membershipParallel.c
+++ membershipParallel.c
@@ -6,7 +6,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <omp_lib.h>
+#include <omp.h>
 
 State* findState(StateContainer* states, State* s){
     for(int i=0; i<states->nStates;i++){
@@ -20,30 +20,34 @@ State* findState(StateContainer* states, State* s){
 
 bool tryPath(ODD* odd, int* numSeq, State* state, Layer* layer, int seq){
 
-    Transition t;
+    Transition* t;
+    int result = 0;
 
+    #pragma omp parallel for
     //TODO Replace for loop with parallelized calls to tryPath
     for(int i=0; i < layer->transitions.nTransitions; i++){
 
-        t = layer->transitions.set[i];
+        t = &(layer->transitions.set[i]);
 
-        if(t.s1 == *state && t.a == numSeq[seq]){
+        if(t->s1 == *state && t->a == numSeq[seq]){
             if(layer->finalFlag){
-                if(findState(&(layer->finalStates),&t.s2)==NULL){
+                if(findState(&(layer->finalStates),&t->s2)==NULL){
                     continue;
                 }
                 else{
-                    return 1;
+                    #pragma omp critical
+                    result = 1;
                 }
 
             }
-            bool b = tryPath(odd, numSeq, &t.s2, &(odd->layerSequence[seq+1]), seq+1);
+            bool b = tryPath(odd, numSeq, &t->s2, &(odd->layerSequence[seq+1]), seq+1);
             if(b){
-                return 1;
+                #pragma omp critical
+                result = 1;
             }
         }
     }
-    return 0;
+    return result;
 }
 
 bool numMembership(ODD* odd, int* numSeq){
@@ -51,18 +55,18 @@ bool numMembership(ODD* odd, int* numSeq){
     Layer* initial = &(odd -> layerSequence[0]);
     //TODO Replace for loop with parallelized calls to tryPath
     OMP_SET_NUM_THREADS(initial->initialStates . nStates);
-
-    #pragma omp parallel{
+    int result = 0;
+    #pragma omp parallel
+    {
         int myID = OMP_GET_THREAD_NUM();
+        printf("hello from %d \n", myID);
         bool b = tryPath(odd, numSeq, &(initial->initialStates.set[myID]), initial, 0);
         if(b){
-            return 1;
-        }
-        #pragma omp barrier;
-        if(myID == 0){
-            return 0;
+            #pragma omp critical
+            result = 1;
         }
     }
+    return result;
 }
 
 
@@ -74,7 +78,7 @@ bool tryStringPath(ODD* odd, char** strSeq, State* state, Layer* layer, int seq)
     int partition;
     int lowerBound;
     int upperBound;
-    Transition t;
+    Transition* t;
 
     lowerBound = 0;
     upperBound = layer->map.sizeAlphabet;
@@ -103,28 +107,31 @@ bool tryStringPath(ODD* odd, char** strSeq, State* state, Layer* layer, int seq)
         partition = (upperBound + lowerBound) / 2;
     }
 
-
+    int result = 0;
+    #pragma omp parallel for
     //TODO Replace for loop with parallelized calls to tryPath
     for(int i=0; i < layer->transitions.nTransitions; i++){
 
-        t = layer->transitions.set[i];
+        t = &(layer->transitions.set[i]);
 
-        if(t.s1 == *state && t.a == currentInt){
+        if(t->s1 == *state && t->a == currentInt){
             if(layer->finalFlag){
-                if(findState(&(layer->finalStates),&t.s2)==NULL){
-                    return 0;
+                if(findState(&(layer->finalStates),&t->s2)==NULL){
+                    continue;
                 }
                 else{
-                    return 1;
+                    #pragma omp critical
+                    result = 1;
                 }
             }
-            bool b = tryStringPath(odd, strSeq, &t.s2, &(odd->layerSequence[seq+1]), seq+1);
+            bool b = tryStringPath(odd, strSeq, &(t->s2), &(odd->layerSequence[seq+1]), seq+1);
             if(b){
-                return 1;
+                #pragma omp critical
+                result = 1;
             }
         }
     }
-    return 0;
+    return result;
 }
 
 bool strMembership(ODD* odd, char** strSeq){
@@ -133,16 +140,16 @@ bool strMembership(ODD* odd, char** strSeq){
 
     //TODO Replace for loop with parallelized calls to tryPath
     OMP_SET_NUM_THREADS(initial->initialStates . nStates);
-
-    #pragma omp parallel{
+    int result = 0;
+    #pragma omp parallel
+    {
         int myID = OMP_GET_THREAD_NUM();
         bool b = tryStringPath(odd, strSeq, &(initial->initialStates.set[myID]), initial, 0);
         if(b){
-            return 1;
-        }
-    #pragma omp barrier;
-        if(myID == 0){
-            return 0;
+            #pragma omp critical
+            result = 1;
         }
     }
+    return result;
+
 }
