4c51063c2f621050ce0560a9a322a48222249a88
aom035 <aom035@student.uib.no>
2019-03-16 22:28:54 +0100

    Kokoro goes doki doki, main goes zabu zabu

--- main.c
+++ main.c
@@ -1,38 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 #include "odd.h"
-#include "debugTools.h"
-#include "cloneKill.h"
-
-int main() {
-
-    char filename[] = "ODD-Example.txt";
-//    char filename[] = "ODD-Memo-Mapping.txt";
-    ODD *odd = malloc(sizeof(ODD));
-    readODD(filename, odd);
-
-    ODD *clone = cloneODD(odd);
-//    Layer *inputLayer = &odd->layerSequence[3];
-//    Layer *resultLayer = malloc(sizeof(Layer));
 
 
-    printf("\n\nBEFORE MEMO (original)\n\n");
-    showODD(odd);
-//    showLayer(&odd->layerSequence[1]);
-//    odd->layerSequence[1] = *memorizeLayer(&odd->layerSequence[1]);
-    ODD* memorized = memorizeODD(clone);
-    printf("\n\nAFTER MEMO (clone)\n\n");
-//    showLayer(&odd->layerSequence[1]);
-    showODD(memorized);
-
-//    printf("\n\nDOUBLE CHECK ORIGINAL\n\n");
-//    showODD(odd);
-
-//    showODD(clone);
-
-    printf("*odd: %p\n"
-           "*clone: %p\n"
-           "Equal: %s\n", odd, clone, (odd == clone) ? "YES" : "NO");
+int main(){
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
     return 0;
 }
