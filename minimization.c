bd84f77e78cc1089fec1c7bc2d6e3e7277832e7d
Daniel Lansdaal <daniellansdaal@gmail.com>
2019-03-20 16:57:23 +0100

    minimize with new definition for removing dead states and transitions left to right. Compilation warnings need to be addressed in actual minimize function

--- minimization.c
+++ minimization.c
@@ -3,154 +3,232 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "odd.h"
+#include "debugTools.h"
 #include <omp.h> //also add -fopenmp to compile path
 
-Layer deleteDeadStatesLeft(Layer* layer, int* livingCount, int* livingStates, Layer l) {
-    Transition* transitions = layer->transitions.set;
-    int numTransitions = layer->transitions.nTransitions;
-    int numStates = layer->leftStates.nStates;
-    State * states = layer->leftStates.set;
-    *livingCount = 0;
-    //can make faster by running through only transitions
-    //go through all transitions and add states on the right into new array make sure you dont double up
-    //if you can ensure transitions come from a valid left state (which should be the case as we deleted dead states from the left)
+Layer removeTransitions(Layer l);
 
-    //good idea but how do we avoid getting double ups?
+Layer setUpInitialLayer(Layer l) {
+    //remove dead left states in first layer and put it in new layer
+    Layer newLayer;
+    newLayer.leftStates = l.initialStates;
+    newLayer.initialStates = l.initialStates;
+    newLayer.finalFlag = l.finalFlag;
+    newLayer.finalStates = l.finalStates;
+    newLayer.initialFlag = l.initialFlag;
+    newLayer.map = l.map;
+    newLayer.rightStates = l.rightStates;
+    newLayer.width = l.width;
+    fprintf(stderr, "problem?");
+    newLayer = removeTransitions(newLayer);
+    fprintf(stderr, "now?");
 
-    #pragma omp parallel for
+    //free(&l);
+    return newLayer;
+
+}
+
+Layer removeDeadRightStates(Layer l) {
+   //go through all right states
+   State* rightStates = l.rightStates.set;
+   int numStates = l.rightStates.nStates;
+
+   Transition* transitions = l.transitions.set;
+   int numTransitions = l.transitions.nTransitions;
+
+   long count = 0;
+   //go through transitions until find that state
+   for (int i = 0; i < numStates; i++) {
+       bool found = false;
+       for (int j = 0; j < numTransitions; j++) {
+           //if you find it, change boolean
+           if (rightStates[i] == transitions[j].s2) {
+               found = true;
+               break;
+           }
+       }
+       if (found) {
+           count++;
+       }
+       else {
+           rightStates[i] = -1;
+       }
+   }
+
+    State* newStates = malloc (count * sizeof(State));
+    count = 0;
     for (int i = 0; i < numStates; i++) {
-        for (int j = 0; j < numTransitions; j++) {
-            if (transitions[j].s2 == states[i]) {
-                livingStates[*livingCount] = states[i];
-                *livingCount++;
+        if (rightStates[i] != -1) {
+            newStates[count] = rightStates[i];
+            count++;
+        }
+    }
+
+    free(l.rightStates.set);
+    l.rightStates.set = newStates;
+    //free(&(l.rightStates.nStates));
+    l.rightStates.nStates = count;
+
+    return l;
+   
+}
+
+Layer removeTransitions(Layer l) {
+    Transition* transitions = l.transitions.set;
+    int numTransitions = l.transitions.nTransitions;
+
+    State* leftStates = l.leftStates.set;
+    int numStates = l.leftStates.nStates;
+
+    fprintf(stderr, "after here?");
+
+    long count = 0;
+    //go through all transitions
+    for (int i = 0; i < numTransitions; i++) {
+        bool valid = false;
+        //go through all states and check if s1 of transition is in states
+        for (int j = 0; j < numStates; j++) {
+            if (transitions[i].s1 == leftStates[j]) {
+                //if it is change boolean
+                valid = true;
                 break;
             }
+            fprintf(stderr, "that for loop?");
+        }
+        //if valid increase count
+        if (valid) {
+            count++;
+        }
+        //otherwise set s1 to -1 so we dont add it
+        else {
+            fprintf(stderr, "this must be it %i", i);
+            transitions[i].s1 = -1;
+        }
+        fprintf(stderr, "do you get here?");
+    }
+
+    fprintf(stderr, "whats wrong?");
+
+    Transition* newTrans = malloc(count * sizeof(Transition));
+    count = 0;
+    for (int i = 0; i < numTransitions; i++) {
+        if (transitions[i].s1 != -1) {
+            //add
+            newTrans[count] = transitions[i];
+            count++;
         }
     }
 
-    l.leftStates.set = livingStates;
-    l.leftStates.nStates = livingCount;
-    return l;
+    free(l.transitions.set);
+    l.transitions.set = newTrans;
+    //free(&(l.transitions.nTransitions));
+    l.transitions.nTransitions = count;
     
 }
 
-Layer deleteDeadTransitionsLeft(Layer* layer, int livingCount, int* livingStates, int* livingTransCount, Transition* livingTransitions, Layer l) {
-    Transition* transitions = layer->transitions.set;
-    int transCount = layer->transitions.nTransitions;
-    *livingTransCount = 0;
-    //can do in n*logn time because sorted
-    //binary search??
 
-    #pragma omp parallel for
-    for (int i = 0; i < livingCount; i++) {
-        for (int j = 0; j < transCount; j++) {
-            if (transitions[j].s1 == livingStates[i]) {
-                livingTransitions[*livingTransCount] = transitions[j];
-                *livingTransCount++;
-            }
-        }
+ODD* removeDeadStatesLeftToRight(ODD* odd) {
+    //create new odd to return
+    ODD* newOdd = malloc(sizeof(ODD));
+    newOdd->nLayers = odd->nLayers;
+    newOdd->layerSequence = malloc(newOdd->nLayers * sizeof(Layer));
+
+        fprintf(stderr, "%i\n", newOdd->nLayers);
+    //delete dead initial states and transitions
+    Layer l1 = odd->layerSequence[0];
+    fprintf(stderr, "ello");
+    newOdd->layerSequence[0] = setUpInitialLayer(odd->layerSequence[0]);
+    fprintf(stderr, "govner");
+    for (int i = 0; i < newOdd->nLayers-1; i++) {
+        //check right states
+        fprintf(stderr, "a\n");
+        Layer cur = newOdd->layerSequence[i];
+        //delete dead right states
+        cur = removeDeadRightStates(cur);
+        fprintf(stderr, "b\n");
+        newOdd->layerSequence[i] = cur;
+        //new layer
+        cur = odd->layerSequence[i+1];
+        //set left states to right states of previous layer
+        cur.leftStates = newOdd->layerSequence[i].rightStates;
+        //delete transitions
+        cur = removeTransitions(cur);
+        fprintf(stderr, "c\n");
+        newOdd->layerSequence[i+1] = cur;
+        //repeat
     }
 
-    l.leftStates.set = livingTransitions;
-    l.transitions.nTransitions = livingTransCount;
+    free(odd);
+
+    return newOdd;
+
 }
 
-Layer deleteDeadStatesRight(Layer* layer, int* livingCount, int* livingStates, Layer l) {
-    Transition* transitions = layer->transitions.set;
-    int numTransitions = layer->transitions.nTransitions;
-    int numStates = layer->rightStates.nStates;
-    State * states = layer->rightStates.set;
-    *livingCount = 0;
-    //can make faster by running through only transitions
-    //go through all transitions and add states on the right into new array make sure you dont double up
-    //if you can ensure transitions come from a valid left state (which should be the case as we deleted dead states from the left)
 
-    //good idea but how do we avoid getting double ups?
-
+// int main(int argc, char** argv) {
+//     ODD* odd = malloc(sizeof(ODD));
+//     readODD("ODD-Example.txt", odd);
     
-    #pragma omp parallel for
-    for (int i = 0; i < numStates; i++) {
-        for (int j = 0; j < numTransitions; j++) {
-            if (transitions[j].s2 == states[i]) {
-                livingStates[*livingCount] = states[i];
-                *livingCount++;
-                break;
-            }
-        }
-    }
+//     showODD(odd);
 
-    l.rightStates.set = livingStates;
-    l.rightStates.nStates = livingCount;
-    return l;
-    
-}
+//     odd = removeDeadStatesLeftToRight(odd);
 
-Layer deleteDeadTransitionsRight(Layer* layer, int livingCount, int* livingStates, int* livingTransCount, Transition* livingTransitions, Layer l) {
-    Transition* transitions = layer->transitions.set;
-    int transCount = layer->transitions.nTransitions;
-    *livingTransCount = 0;
-    //can do in n*logn time because sorted
-    //binary search??
-    
-    #pragma omp parallel for
-    for (int i = 0; i < livingCount; i++) {
-        for (int j = 0; j < transCount; j++) {
-            if (transitions[j].s1 == livingStates[i]) {
-                livingTransitions[*livingTransCount] = transitions[j];
-                *livingTransCount++;
-            }
-        }
-    }
+//     showODD(odd);
+// }
 
-    l.transitions.set = livingTransitions;
-    l.transitions.nTransitions = livingTransCount;
-}
+
+
 
 
 Layer minimizeLayer(Layer * layer,Layer l,int i){ //why do we need int i ?  
     #pragma omp parallel for
-    for(int j = 0; j < layer.nstates;j++){  //assume states are sorted   
-        State* state_j = layer.leftStates[j];
+    for(int j = 0; j < layer->leftStates.nStates;j++){  //assume states are sorted   
+        State* state_j = layer->leftStates.set[j];
         if (state_j != -1){
-            for(int k = j; k < layer.nstates,j++){ //not sure about that ??
-                State* state_k = layer.leftStates[k];
+            for(int k = j; k < layer->leftStates.nStates;j++){ //not sure about that ??
+                State* state_k = layer->leftStates.set[k];
                 //check whether j and k are of same type if true they have the same type  
-                if checkType(&state_j,&state_k){
-                    collapseLeft(&layer,&state_j,&state_k)   //where do we put right collapse ?  
+                if (checkType(&state_j,&state_k)){
+                    collapseLeft(&layer,&state_j,&state_k);   //where do we put right collapse ?  
                 }
             } 
         }
     }
-    return l
+    return l;
 }
 
 //takes tranitions not states as we also need NumSymbol
 bool checkTypeLeft(Transition * transition_j,Transition * transition_k){
-    if(transition_j.s1 == transition_k.s1)
-        if(transition_j.a == transition_k.a)
+    if(transition_j->s1 == transition_k->s1)
+        if(transition_j->a == transition_k->a)
             return 1;
     return 0;
 }
 bool checkTypeRight(Transition * transition_j,Transition * transition_k){
-    if(transition_j.s2 == transition_k.s2)
-        if(transition_j.a == transition_k.a)
+    if(transition_j->s2 == transition_k->s2)
+        if(transition_j->a == transition_k->a)
             return 1;
     return 0;
 }
 
-void collapseLeft(Layer * layer, State * state_i, State * state_j){
-    if(state_i < state_j){ //to ensure that state_i < state_j 
-        State tmp =  state_i;
-        state_i = state_j;
+void collapseLeft(Layer * layer, Transition * t_i, Transition * t_j){
+    State state_i;
+    State state_j;
+    if(t_i->s1 < t_j->s1){ //to ensure that state_i < state_j 
+        State tmp =  t_i->s1;
+        state_i = t_i->s1;
         state_j = tmp;
         }
 
-    layer.leftStates[state_j] = -1;
+    layer->leftStates.set[state_i] = -1;
     //get num symbol of state to be replaced -> go to left state, 
-    for (int i = 0; i < n; i++){ //to what does this go ? This is all transitions from 
-        //state x to state j. We assume the states are sorted
 
-        Transition.s1[state_j] = Transition.s1[state_i];
+    //DANIEL changed the n in this for loop to nTransitions. Think its right?
+
+    for (int i = 0; i < layer->transitions.nTransitions; i++){ //to what does this go ? This is all transitions from 
+        //state x to state j. We assume the states are sorted
+        
+        //Transition.s1[state_j] = Transition.s1[state_i];
     }    
 }
 
@@ -162,12 +240,12 @@ void collapseRight(Layer * layer, State * state_i, State * state_j){
         state_j = tmp;
         }
 
-    layer.rightStates[state_j] = -1;
+    layer->rightStates.set[*state_j] = -1;
     //get num symbol of state to be replaced -> go to left state, 
-    for (int i = 0; i < n; i++){ //to what does this go ? This is all transitions from 
+    for (int i = 0; i < layer->transitions.nTransitions; i++){ //to what does this go ? This is all transitions from 
         //state x to state j. We assume the states are sorted
 
-        Transition.s2[state_j] = Transition.s2[state_i];
+        //Transition.s2[state_j] = Transition.s2[state_i];
     }    
 }
 
@@ -186,10 +264,10 @@ ODD* minimize (ODD* odd) {
     // ODD newODD = *p;
 
     //maybe this is better to define new ODD
-    ODD o;
-    o.nLayers = odd->nLayers;
+    ODD* o;
+    o->nLayers = odd->nLayers;
 
-    o.layerSequence = malloc(o.nLayers * sizeof(Layer));
+    o->layerSequence = malloc(o->nLayers * sizeof(Layer));
 
     //better to create new ODD and add layers to it like we thought orignally LOL
 
@@ -208,27 +286,7 @@ ODD* minimize (ODD* odd) {
     Transition* livingTransitions =  currLayer.transitions.set;
     int livingTransCount = currLayer.transitions.nTransitions;
 
-    for (int i = 0; i < numLayers; i++) {
-        l.leftStates.set = livingStates;
-        l.leftStates.nStates = livingCount;
-        l = deleteDeadTransitionsRight(&currLayer, &livingCount, livingStates, &livingTransCount, livingTransitions, l);
-        l = deleteDeadStatesRight(&currLayer, &livingCount, livingStates, l);
-
-        l.width = l.leftStates.nStates > l.rightStates.nStates ? l.leftStates.nStates : l.rightStates.nStates;
-
-        o.layerSequence[i] = l;
-    }
-
-    for (int i = numLayers-1; i >= 0; i--) {
-        l.rightStates.set = livingStates;
-        l.rightStates.nStates = livingCount;
-        l = deleteDeadTransitionsLeft(&currLayer, &livingCount, livingStates, &livingTransCount, livingTransitions, l);
-        l = deleteDeadStatesLeft(&currLayer, &livingCount, livingStates, l);
-
-        l.width = l.leftStates.nStates > l.rightStates.nStates ? l.leftStates.nStates : l.rightStates.nStates;
-
-        o.layerSequence[i] = l;
-    }
+    o = removeDeadStatesLeftToRight(odd);
 
     //reset width to new width
     for(int i = 0; i < odd->nLayers; i++) {
@@ -242,7 +300,7 @@ ODD* minimize (ODD* odd) {
     void sortAllRightTransitions(ODD* odd);
     
     for (int i = 0; i < odd->nLayers; i++){
-        l = minimizeLayer(&odd.layerSequence,i);    
+        l = minimizeLayer(odd->layerSequence,odd->layerSequence[i], i);    
         //get new trimmed Layer without the -1 states 
         
     }
