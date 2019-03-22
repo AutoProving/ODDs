00da28e83ff45dc158d147dc4d191fd6d8cb0045
TooLazyToMakeAName <sklone96@gmail.com>
2019-03-22 21:44:25 +0100

    updated to fancy NxN -> N mapping

--- tensorProduct.c
+++ tensorProduct.c
@@ -4,13 +4,28 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+int  cantorPairing(int a, int b){
+    return b+(((a+b)*(a+b+1))/2);
+};
+
+int max(int* list, int size){
+    int max = list[0];
+    for (int i = 1; i < size; ++i) {
+       if (max<list[i]){
+       	max = list[i];
+       }
+    }
+	return max;
+}
+
 void tensorStates(StateContainer* stateContainer1, StateContainer* stateContainer2, StateContainer* result){
 
 		int nStates = (stateContainer1->nStates)*(stateContainer2->nStates);
 		State* set = malloc(sizeof(State)*nStates);
 		for (int i = 0; i < stateContainer1->nStates; ++i) {
 			for (int j = 0;  j < stateContainer2->nStates; ++j) {
-				set[i*stateContainer1->nStates+j] = i*stateContainer1->nStates+j;
+				set[i*stateContainer1->nStates+j] = cantorPairing(stateContainer1->set[i], stateContainer2->set[j]);
 			}
 		}
 		result->nStates = nStates;
@@ -18,45 +33,39 @@ void tensorStates(StateContainer* stateContainer1, StateContainer* stateContaine
 }
 
 void tensorEdgeStates(StateContainer* states1, StateContainer* edgeStates1, StateContainer* states2, StateContainer* edgeStates2, StateContainer* result){
-	int fullStateSize = states1->nStates*states2->nStates;
+	int fullStateSize = max(states1->set, states1->nStates)*max(states2->set, states2->nStates);
 	int* countSortSet = malloc(sizeof(int)*fullStateSize);
 	for (int i = 0; i <fullStateSize; ++i) {
 		countSortSet[i] = 0;
 	}
 
-	int width = states2->nStates;
+	int nStates = 0;
 
 	for (int i = 0; i < edgeStates1->nStates; ++i) {
-		int l1InitState = edgeStates1->set[i];
 		for (int j = 0; j < states2->nStates; ++j) {
-			countSortSet[l1InitState*width+j]+=1;
+			int index = cantorPairing(edgeStates1->set[i],states2->set[j]);
+			if (countSortSet[index]==0){
+				nStates+=1;
+			}
 		}
 	}
 
 	for (int j = 0; j < edgeStates2->nStates; ++j) {
-		int l2InitState = edgeStates2->set[j];
 		for (int i = 0; i < states1->nStates; ++i) {
-			countSortSet[i*width+l2InitState]+=1;
-		}
-	}
-	int nStates = 0;
-	for (int i = 0; i < states1->nStates; ++i) {
-		for (int j = 0; j < states2->nStates; ++j) {
-			if(countSortSet[i*width+j] >0){
-				nStates +=1;
+			int index = cantorPairing(states1->set[i],edgeStates2->set[j]);
+			if (countSortSet[index]==0){
+				nStates+=1;
 			}
 		}
 	}
 
 	State* set = malloc(sizeof(State)*nStates);
 
-	nStates = 0;
-	for (int i = 0; i < states1->nStates; ++i) {
-		for (int j = 0; j < states2->nStates; ++j) {
-			if(countSortSet[i*width+j] >0){
-				set[nStates] = i*width+j;
-				nStates +=1;
-			}
+	int setIndex = 0;
+	for (int i = 0; i < fullStateSize; ++i) {
+		if (countSortSet[i]==1){
+			set[setIndex] = i;
+			setIndex+= 1;
 		}
 	}
 
@@ -76,9 +85,10 @@ Layer* tensorLayers(Layer* layer1, Layer* layer2) {
 			char* l2Symbol = layer2->map.N2S[j];
 			int l2Length = strlen(l2Symbol);
 			int length = l1Length+l2Length;
-			char* newSymbol = malloc(sizeof(char)*(length+1));
+			char* newSymbol = malloc(sizeof(char)*(length+2));
 			strncpy(newSymbol,l1Symbol,l1Length);
-			strncpy(&newSymbol[l1Length],l2Symbol,l2Length+1);
+			newSymbol[l1Length] ="|";
+			strncpy(&newSymbol[l1Length+1],l2Symbol,l2Length+1);
 			map.N2S[i*layer2->map.sizeAlphabet+j]=newSymbol;
 			map.S2N[i*layer2->map.sizeAlphabet+j]=i*layer2->map.sizeAlphabet+j;
 		}
@@ -113,15 +123,17 @@ Layer* tensorLayers(Layer* layer1, Layer* layer2) {
 
 
 
+	//use fancy new function
+
 	int nTransitions = layer1->transitions.nTransitions*layer2->transitions.nTransitions;
 	Transition* set = malloc(sizeof(Transition)*nTransitions);
 	for(int i = 0; i< layer1->transitions.nTransitions; ++i){
 		Transition l1Trance = layer1->transitions.set[i];
 		for(int j = 0; j< layer2->transitions.nTransitions; ++j){
 			Transition l2Trace = layer2->transitions.set[j];
-			State s1 = l1Trance.s1*layer2->leftStates.nStates+l2Trace.s1;
+			State s1 = cantorPairing(l1Trance.s1, l2Trace.s1);
 			NumSymbol  a = l1Trance.a*layer2->map.sizeAlphabet+l2Trace.a;
-			State s2 = l1Trance.s2*layer2->rightStates.nStates+l2Trace.s2;
+			State s2 = cantorPairing(l1Trance.s2, l2Trace.s2);
 			Transition t = {s1,a,s2};
 			set[i*layer2->transitions.nTransitions+j]= t;
 		}
