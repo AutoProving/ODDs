05918909e53d2fe055ada31793d59475dd02eb6d
Vasily Alferov <vasily.v.alferov@gmail.com>
2020-01-13 18:32:28 +0100

    ODDs::ODD and ODDs::ODDBuilder classes

--- lib/ODDs.cpp
+++ lib/ODDs.cpp
@@ -22,4 +22,117 @@ bool ODD::Transition::operator<(const ODD::Transition& rhs) const {
     return to < rhs.to;
 }
 
+int ODD::AlphabetMap::addSymbol(const std::string& symbol) {
+    auto it = s2n_.lower_bound(symbol);
+    if (it != s2n_.end() && it->first == symbol) {
+        return it->second;
+    }
+    int newId = n2s_.size();
+    s2n_.insert(it, std::make_pair(symbol, newId));
+    n2s_.push_back(symbol);
+    return newId;
+}
+
+const std::string& ODD::AlphabetMap::numberToSymbol(int number) const {
+    return n2s_[number];
+}
+
+int ODD::AlphabetMap::symbolToNumber(const std::string& symbol) const {
+    auto it = s2n_.find(symbol);
+    if (it == s2n_.end())
+        return -1;
+    return it->second;
+}
+
+int ODD::Layer::width() const {
+    return std::max<int>(leftStates->size(), rightStates->size());
+}
+
+bool ODD::Layer::checkSanity() const {
+    if (!isInitial && !initialStates.empty())
+        return false;
+    if (!isFinal && !finalStates.empty())
+        return false;
+    return true;
+}
+
+ODD::ODD() = default;
+
+int ODD::countLayers() const {
+    return layers_.size();
+}
+
+const ODD::Layer& ODD::getLayer(int i) const {
+    return layers_[i];
+}
+
+class ODDBuilder::Impl {
+public:
+    Impl(const ODD::StateContainer& firstLayerLeft)
+        : states_({firstLayerLeft})
+    {}
+
+    void addLayer(const ODD::AlphabetMap& alphabet,
+                  const ODD::TransitionContainer& transitions,
+                  const ODD::StateContainer& rightStates) {
+        alphabets_.push_back(alphabet);
+        transitions_.push_back(transitions);
+        states_.push_back(rightStates);
+    }
+
+    void setInitialStates(const ODD::StateContainer& initialStates) {
+        initialStates_ = initialStates;
+    }
+
+    void setFinalStates(const ODD::StateContainer& finalStates) {
+        finalStates_ = finalStates;
+    }
+
+    ODD build() {
+        ODD ret;
+        ret.states_ = std::move(states_);
+        ret.layers_.resize(alphabets_.size());
+        for (int i = 0; i < (int)alphabets_.size(); i++) {
+            ret.layers_[i].alphabet = std::move(alphabets_[i]);
+            ret.layers_[i].leftStates = &ret.states_[i];
+            ret.layers_[i].rightStates = &ret.states_[i + 1];
+            ret.layers_[i].transitions = transitions_[i];
+            ret.layers_[i].isInitial = i == 0;
+            ret.layers_[i].isFinal = i + 1 == (int)ret.states_.size();
+        }
+        ret.layers_[0].initialStates = std::move(initialStates_);
+        ret.layers_.back().finalStates = std::move(finalStates_);
+        return ret;
+    }
+
+private:
+    std::vector<ODD::StateContainer> states_;
+    std::vector<ODD::AlphabetMap> alphabets_;
+    std::vector<ODD::TransitionContainer> transitions_;
+    ODD::StateContainer initialStates_;
+    ODD::StateContainer finalStates_;
+};
+
+ODDBuilder::ODDBuilder(const ODD::StateContainer& firstLayerLeft)
+    : impl_(std::make_unique<ODDBuilder::Impl>(firstLayerLeft))
+{}
+
+void ODDBuilder::addLayer(const ODD::AlphabetMap& alphabet,
+                          const ODD::TransitionContainer& transitions,
+                          const ODD::StateContainer& rightStates) {
+    impl_->addLayer(alphabet, transitions, rightStates);
+}
+
+void ODDBuilder::setInitialStates(const ODD::StateContainer& initialStates) {
+    impl_->setInitialStates(initialStates);
+}
+
+void ODDBuilder::setFinalStates(const ODD::StateContainer& finalStates) {
+    impl_->setFinalStates(finalStates);
+}
+
+ODD ODDBuilder::build() {
+    return impl_->build();
+}
+
 }
--- lib/ODDs.h
+++ lib/ODDs.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <vector>
+
 namespace ODDs {
 
 class ODD {
@@ -45,6 +51,155 @@ public:
          */
         bool operator<(const Transition& rhs) const;
     };
+
+    /**
+     * @brief A type for state containers.
+     *
+     * The `std::set` implementation might be a subject to library-wide change.
+     * Yet the current implementation may rely on `std::set` features.
+     */
+    using StateContainer = std::set<State>;
+
+    /**
+     * @brief A type for transition containers.
+     *
+     * TODO: Make it a wrapper around `std::multimap`.
+     */
+    using TransitionContainer = std::set<Transition>;
+
+    /**
+     * @brief A mapping between the alphabet and the state.
+     */
+    class AlphabetMap {
+    public:
+        /**
+         * @brief Expand the alphabet.
+         *
+         * @return The new symbol id if {@param symbol} was not in alphabet,
+         * and its id if it was.
+         */
+        int addSymbol(const std::string& symbol);
+
+        /**
+         * @brief  Get the symbol.
+         *
+         * The behaviour in case the number is not in alphabet is undefined.
+         */
+        const std::string& numberToSymbol(int number) const;
+
+        /**
+         * @brief  Get the symbol id.
+         * @return Symbol id, or -1 in case the symbol is not mapped.
+         */
+        int symbolToNumber(const std::string& symbol) const;
+
+    private:
+        std::vector<std::string> n2s_;
+        std::map<std::string, int> s2n_;
+    };
+
+    /**
+     * @brief A type for layers.
+     */
+    struct Layer {
+        AlphabetMap alphabet;
+        StateContainer *leftStates; // Owned by the ODD
+        StateContainer initialStates;
+        StateContainer *rightStates; // Owned by the ODD
+        StateContainer finalStates;
+        TransitionContainer transitions;
+        bool isInitial, isFinal;
+
+        /**
+         * @brief Width of the layer.
+         *
+         * Equal to `max(leftStates.size(), rightStates.size())`.
+         */
+        int width() const;
+
+        /**
+         * @brief Checks correctness of boolean flags.
+         *
+         * Checks that values of isInitial and isFinal correspond with
+         * initialStates and finalStates.
+         *
+         * @return True if flags are set correctly, false otherwise.
+         */
+        bool checkSanity() const;
+    };
+
+private:
+    ODD(); // Use ODDBuilder to construct ODDs
+
+public:
+    /**
+     * @brief Returns number of layers.
+     *
+     * This is the number of transition sets, not state sets.
+     */
+    int countLayers() const;
+
+    /**
+     * @brief Constant reference to the layer by its id.
+     */
+    const Layer& getLayer(int i) const;
+
+private:
+    std::vector<StateContainer> states_;
+    std::vector<Layer> layers_;
+
+    friend class ODDBuilder;
+};
+
+/**
+ * @brief A builder for ODDs.
+ *
+ * Simple alternative to a monsterous constructor. Guarantees by design that
+ * the built ODD is correct.
+ * 
+ * TODO: Provide an example.
+ */
+class ODDBuilder {
+public:
+    /**
+     * @brief Construct from the leftmost set of states.
+     */
+    ODDBuilder(const ODD::StateContainer& firstLayerLeft);
+
+    ~ODDBuilder() = default;
+    ODDBuilder(const ODDBuilder&) = delete;
+    ODDBuilder& operator=(const ODDBuilder&) = delete;
+    ODDBuilder(ODDBuilder&&) = default;
+    ODDBuilder& operator=(ODDBuilder&&) = default;
+
+    /**
+     * @brief Add a new layer to the right.
+     */
+    void addLayer(const ODD::AlphabetMap& alphabet,
+                  const ODD::TransitionContainer& transitions,
+                  const ODD::StateContainer& rightStates);
+
+    /**
+     * @brief Set the initial states set for the leftmost layer.
+     */
+    void setInitialStates(const ODD::StateContainer& initialStates);
+
+    /**
+     * @brief Set the final states set for the rightmost layer.
+     */
+    void setFinalStates(const ODD::StateContainer& finalStates);
+
+    /**
+     * @brief Build the ODD.
+     *
+     * @warning ODDBuilder isn't supposed to be used after this function
+     * is called. It may leave the builder in unconsistent state.
+     */
+    ODD build();
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
 };
 
 }
