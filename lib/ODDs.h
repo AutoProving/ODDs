73316412af5bb50dd5bf8b3b2d4e48d4601cb82c
Vasily Alferov <vasily.v.alferov@gmail.com>
2020-01-13 15:05:38 +0100

    Add docs

--- lib/ODDs.h
+++ lib/ODDs.h
@@ -2,4 +2,20 @@
 
 namespace ODDs {
 
+/**
+ * @brief A type for symbol indices.
+ *
+ * Since this type is not a likely subject to change, this should be defined
+ * as a type synonim rather then a template parameter for the ODD class.
+ */
+using Symbol = int;
+
+/**
+ * @brief A type for state indices.
+ *
+ * Since this type is not a likely subject to change, this should be defined
+ * as a type synonim rather then a template parameter for the ODD class.
+ */
+using State = int;
+
 }
