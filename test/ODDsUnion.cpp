adef6fd711b44b60137dd1ba624692718adb1f64
Vasily Alferov <vasily.v.alferov@gmail.com>
2020-01-15 17:34:02 +0100

    One more union test

--- test/ODDsUnion.cpp
+++ test/ODDsUnion.cpp
@@ -25,6 +25,10 @@ protected:
     static std::string trivialLhsDesc;
     static std::string trivialRhsDesc;
     static std::string trivialExpectedDesc;
+
+    static std::string nonDetLhsDesc;
+    static std::string nonDetRhsDesc;
+    static std::string nonDetExpectedDesc;
 };
 
 // {abc,abd}, not minimal
@@ -130,4 +134,116 @@ std::string ODDsUnionTest::trivialExpectedDesc = R"(
 
 TEST_F(ODDsUnionTest, trivial) {
     doTest(trivialLhsDesc, trivialRhsDesc, trivialExpectedDesc);
+}
+
+//{abc,abd} non-deterministic
+std::string ODDsUnionTest::nonDetLhsDesc = R"(
+{
+  "leftLayerStates": 1,
+  "initialStates": [0],
+  "layers": [
+    {
+      "alphabet": ["a"],
+      "transitions": [
+        {"from": 0, "symbol": "a", "to": 0},
+        {"from": 0, "symbol": "a", "to": 1}
+      ],
+      "rightLayerStates": 2
+    },
+    {
+      "alphabet": ["b"],
+      "transitions": [
+        {"from": 0, "symbol": "b", "to": 0},
+        {"from": 1, "symbol": "b", "to": 1}
+      ],
+      "rightLayerStates": 2
+    },
+    {
+      "alphabet": ["c", "d"],
+      "transitions": [
+        {"from": 0, "symbol": "c", "to": 0},
+        {"from": 1, "symbol": "d", "to": 1}
+      ],
+      "rightLayerStates": 2
+    }
+  ],
+  "finalStates": [0, 1]
+})";
+
+//{cba,dba} non-deterministic
+std::string ODDsUnionTest::nonDetRhsDesc = R"(
+{
+  "leftLayerStates": 2,
+  "initialStates": [0],
+  "layers": [
+    {
+      "alphabet": ["c", "d", "a"],
+      "transitions": [
+        {"from": 0, "symbol": "c", "to": 0},
+        {"from": 0, "symbol": "d", "to": 0},
+        {"from": 1, "symbol": "a", "to": 0}
+      ],
+      "rightLayerStates": 1
+    },
+    {
+      "alphabet": ["b"],
+      "transitions": [
+        {"from": 0, "symbol": "b", "to": 0}
+      ],
+      "rightLayerStates": 1
+    },
+    {
+      "alphabet": ["a"],
+      "transitions": [
+        {"from": 0, "symbol": "a", "to": 1},
+        {"from": 0, "symbol": "a", "to": 0}
+      ],
+      "rightLayerStates": 2
+    }
+  ],
+  "finalStates": [1]
+})";
+
+//{abc,abd,cba,cbd} non-deterministic
+std::string ODDsUnionTest::nonDetExpectedDesc = R"(
+{
+  "leftLayerStates": 3,
+  "initialStates": [0, 1],
+  "layers": [
+    {
+      "alphabet": ["a", "c", "d"],
+      "transitions": [
+        {"from": 0, "symbol": "a", "to": 0},
+        {"from": 0, "symbol": "a", "to": 1},
+        {"from": 1, "symbol": "c", "to": 2},
+        {"from": 1, "symbol": "d", "to": 2},
+        {"from": 2, "symbol": "a", "to": 2}
+      ],
+      "rightLayerStates": 3
+    },
+    {
+      "alphabet": ["b"],
+      "transitions": [
+        {"from": 0, "symbol": "b", "to": 0},
+        {"from": 1, "symbol": "b", "to": 1},
+        {"from": 2, "symbol": "b", "to": 2}
+      ],
+      "rightLayerStates": 3
+    },
+    {
+      "alphabet": ["c", "d", "a"],
+      "transitions": [
+        {"from": 0, "symbol": "c", "to": 0},
+        {"from": 1, "symbol": "d", "to": 1},
+        {"from": 2, "symbol": "a", "to": 2},
+        {"from": 2, "symbol": "a", "to": 3}
+      ],
+      "rightLayerStates": 4
+    }
+  ],
+  "finalStates": [0, 1, 3]
+})";
+
+TEST_F(ODDsUnionTest, nonDet) {
+    doTest(nonDetLhsDesc, nonDetRhsDesc, nonDetExpectedDesc);
 }
